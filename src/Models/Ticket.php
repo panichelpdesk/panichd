0e9ba42a85570c78611d82b82e961ebb3256de73
Xavi <xaviqv@tordera.cat>
2017-03-14 10:33:10 +0100

    Models\Ticket: Added scopeTicketList() to wrap Active() and Complete()

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -47,6 +47,18 @@ class Ticket extends Model
     {
         return $query->whereNull('completed_at');
     }
+	
+	/**
+     * Get specified ticket list (active or complete)
+     *
+     * @return Collection
+     */
+	public function scopeTicketList($query,$list)
+	{
+		if ($list=="active") return $query->Active($query);
+		if ($list=="complete") return $query->Complete($query);
+		return false;
+	}
 
     /**
      * Get Ticket status.
@@ -186,6 +198,25 @@ class Ticket extends Model
         });
     }
 	
+	/**
+     * Get all visible tickets for specified username
+     *
+     * @param $query
+     * @param $id
+     *
+     * @return mixed
+     */
+	/*public function scopeVisibleFor($query, $id)
+	{
+		if (Agent::isAdmin()){
+			return $query;
+		}elseif(Agent::isAgent($id)){
+			return $query->VisibleForAgent($id);
+		}else{
+			return $query->UserTickets($id);
+		}
+	}*/
+	 
 	
 	/**
      * Get all visible tickets for agent.
@@ -207,7 +238,7 @@ class Ticket extends Model
 			});
 		}else{
 			// Returns all tickets Owned by Agent with $id only
-			return $query->where('agent_id', $id);
+			return $query->AgentTickets($id);
 		}
     }
 
