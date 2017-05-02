13b233cbf444b05fa5ea0010d62c5fd61ba41672
Xavi <xaviqv@tordera.cat>
2017-05-02 18:31:38 +0200

    Admin. categories: share tag script for edit and create

--- src/Views/admin/category/create.blade.php
+++ src/Views/admin/category/create.blade.php
@@ -2,10 +2,12 @@
 @section('page', trans('ticketit::admin.category-create-title'))
 
 @section('content')
-    <div class="well bs-component">
+    @include('ticketit::shared.header')
+	<div class="well bs-component">
         {!! CollectiveForm::open(['route'=> $setting->grab('admin_route').'.category.store', 'method' => 'POST', 'class' => 'form-horizontal']) !!}
             <legend>{{ trans('ticketit::admin.category-create-title') }}</legend>
             @include('ticketit::admin.category.form')
         {!! CollectiveForm::close() !!}
     </div>
+	@include('ticketit::admin.category.tags_script')
 @stop
--- src/Views/admin/category/edit.blade.php
+++ src/Views/admin/category/edit.blade.php
@@ -13,34 +13,5 @@
         @include('ticketit::admin.category.form', ['update', true])
         {!! CollectiveForm::close() !!}
     </div>
-	<script type="text/javascript">
-	$(function(){
-		$('#admin-select2-tags').select2({
-		  tags: true,
-		  tokenSeparators: [',']
-		});
-		
-		$('.jquery_button_delete_tag').click(function(e){
-			var i=$(this).prop('id').replace('tag_delete_','');
-			$(this).hide();
-			$('#tag_text_'+i).css('text-decoration','line-through').css('background-color','#ff9999');
-			$('#jquery_delete_tag_'+i).prop('disabled',false);
-			
-			$('#tag_keep_'+i).show();
-			
-			e.preventDefault();
-		});
-		
-		$('.jquery_button_keep_tag').click(function(e){
-			var i=$(this).prop('id').replace('tag_keep_','');
-			$(this).hide();
-			$('#tag_text_'+i).css('text-decoration','').css('background-color','');
-			$('#jquery_delete_tag_'+i).prop('disabled',true);
-			
-			$('#tag_delete_'+i).show();
-			
-			e.preventDefault();
-		});
-	});
-	</script>
+	@include('ticketit::admin.category.tags_script')
 @stop
