c5f8432cd38839430db8de6415df604b5aebf1a8
Xavi <xaviqv@tordera.cat>
2017-05-29 16:44:57 +0200

    ticket card: changed button position and design. created and updated info moved to header

--- src/Views/tickets/partials/ticket_body.blade.php
+++ src/Views/tickets/partials/ticket_body.blade.php
@@ -11,54 +11,47 @@
 				</h2>
 			</div>
 			<div class="col-md-4 text-right">
-				@if(! $ticket->completed_at && $close_perm == 'yes')
-							{!! link_to_route($setting->grab('main_route').'.complete', trans('ticketit::lang.btn-mark-complete'), $ticket->id,
-												['class' => 'btn btn-success']) !!}
-					@elseif($ticket->completed_at && $reopen_perm == 'yes')
-							{!! link_to_route($setting->grab('main_route').'.reopen', trans('ticketit::lang.reopen-ticket'), $ticket->id,
-												['class' => 'btn btn-success']) !!}
-					@endif
-					@if($u->isAgent() || $u->isAdmin())
-						<button type="button" class="btn btn-info" data-toggle="modal" data-target="#ticket-edit-modal">
-							{{ trans('ticketit::lang.btn-edit')  }}
+				@if ($ticket->updated_at!=$ticket->created_at)
+				<span class="glyphicon glyphicon-pencil" title="darrer canvi" style="color: #aaa; cursor: help"></span> {{ $ticket->updated_at->diffForHumans() }} 
+				@endif
+				<span class="glyphicon glyphicon-certificate" title="data de creació" style="color: #aaa; cursor: help"></span> {{ $ticket->created_at->diffForHumans() }}&nbsp;
+								
+				@if($u->isAdmin())
+					@if($setting->grab('delete_modal_type') == 'builtin')
+						{!! link_to_route(
+										$setting->grab('main_route').'.destroy', trans('ticketit::lang.btn-delete'), $ticket->id,
+										[
+										'class' => 'btn btn-default deleteit',
+										'form' => "delete-ticket-$ticket->id",
+										"node" => $ticket->subject
+										])
+						!!}
+					@elseif($setting->grab('delete_modal_type') == 'modal')
+					{{-- // OR; Modal Window: 1/2 --}}
+						{!! CollectiveForm::open(array(
+								'route' => array($setting->grab('main_route').'.destroy', $ticket->id),
+								'method' => 'delete',
+								'style' => 'display:inline'
+						   ))
+						!!}
+						<button type="button"
+								class="btn btn-default"
+								data-toggle="modal"
+								data-target="#confirmDelete"
+								data-title="{!! trans('ticketit::lang.show-ticket-modal-delete-title', ['id' => $ticket->id]) !!}"
+								data-message="{!! trans('ticketit::lang.show-ticket-modal-delete-message', ['subject' => $ticket->subject]) !!}"
+						 >
+						  {{ trans('ticketit::lang.btn-delete') }}
 						</button>
 					@endif
-					@if($u->isAdmin())
-						@if($setting->grab('delete_modal_type') == 'builtin')
-							{!! link_to_route(
-											$setting->grab('main_route').'.destroy', trans('ticketit::lang.btn-delete'), $ticket->id,
-											[
-											'class' => 'btn btn-danger deleteit',
-											'form' => "delete-ticket-$ticket->id",
-											"node" => $ticket->subject
-											])
-							!!}
-						@elseif($setting->grab('delete_modal_type') == 'modal')
-						{{-- // OR; Modal Window: 1/2 --}}
-							{!! CollectiveForm::open(array(
-									'route' => array($setting->grab('main_route').'.destroy', $ticket->id),
-									'method' => 'delete',
-									'style' => 'display:inline'
-							   ))
-							!!}
-							<button type="button"
-									class="btn btn-danger"
-									data-toggle="modal"
-									data-target="#confirmDelete"
-									data-title="{!! trans('ticketit::lang.show-ticket-modal-delete-title', ['id' => $ticket->id]) !!}"
-									data-message="{!! trans('ticketit::lang.show-ticket-modal-delete-message', ['subject' => $ticket->subject]) !!}"
-							 >
-							  {{ trans('ticketit::lang.btn-delete') }}
-							</button>
-						@endif
-							{!! CollectiveForm::close() !!}
-						{{-- // END Modal Window: 1/2 --}}
+						{!! CollectiveForm::close() !!}
+					{{-- // END Modal Window: 1/2 --}}
 				@endif
 			</div>
 		</div>
 
 		<div class="row">
-			<div class="col-lg-3 col-sm-4">
+			<div class="col-lg-3 col-sm-4">				
 				<p><strong>{{ trans('ticketit::lang.owner') }}</strong>{{ trans('ticketit::lang.colon') }}{{ $ticket->user->name }}<br />
 				<strong>{{ trans('ticketit::lang.status') }}</strong>{{ trans('ticketit::lang.colon') }}
 					@if( $ticket->isComplete() && ! $setting->grab('default_close_status_id') )
@@ -71,8 +64,7 @@
 						{{ $ticket->priority->name }}
 					</span>
 				</p>
-				<p><strong>{{ trans('ticketit::lang.created') }}</strong>{{ trans('ticketit::lang.colon') }}{{ $ticket->created_at->diffForHumans() }}<br />
-				<strong>{{ trans('ticketit::lang.last-update') }}</strong>{{ trans('ticketit::lang.colon') }}{{ $ticket->updated_at->diffForHumans() }}</p>
+				
 				
 				<p><strong>{{ trans('ticketit::lang.responsible') }}</strong>{{ trans('ticketit::lang.colon') }}{{ $ticket->agent->name }}<br />
 				<strong>{{ trans('ticketit::lang.category') }}</strong>{{ trans('ticketit::lang.colon') }}
@@ -86,11 +78,11 @@
 					@endforeach
 					
 				@endif
-				</p>
+				</p>				
 			</div>
 			<div class="col-lg-9 col-sm-8">
 				<div class="row">
-					<div class="col-md-6">
+					<div class="{{ $ticket->intervention_html ? 'col-md-6' : 'col-md-12'}}">
 						<div class="panel panel-default">
 						<div class="panel-body">
 						<div>
@@ -99,18 +91,33 @@
 						<p> {!! $ticket->html !!} </p>
 						</div></div>
 					</div>
-					<div class="col-md-6">
-						<div class="panel panel-default">
-						<div class="panel-body">
-						<div>
-							<b>Actuació</b>
+					@if ($ticket->intervention_html)
+						<div class="col-md-6">
+							<div class="panel panel-default">
+							<div class="panel-body">
+							<div>
+								<b>Actuació</b>
+							</div>
+							<p> {!! $ticket->intervention_html !!} </p>
+							</div></div>
 						</div>
-						<p> {!! $ticket->intervention_html !!} </p>
-						</div></div>
-					</div>
+					@endif
 				</div>
 			</div>
 		</div>
+		@if(! $ticket->completed_at && $close_perm == 'yes')
+			{!! link_to_route($setting->grab('main_route').'.complete', trans('ticketit::lang.btn-mark-complete'), $ticket->id,
+									['class' => 'btn btn-default']) !!}
+		@elseif($ticket->completed_at && $reopen_perm == 'yes')
+			{!! link_to_route($setting->grab('main_route').'.reopen', trans('ticketit::lang.reopen-ticket'), $ticket->id,
+									['class' => 'btn btn-default']) !!}
+		@endif
+		@if($u->isAgent() || $u->isAdmin())
+			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#ticket-edit-modal">
+				{{ trans('ticketit::lang.btn-edit')  }}
+			</button>
+			<div class="visible-xs"><br /></div>
+		@endif
         
         {!! CollectiveForm::open([
                         'method' => 'DELETE',
