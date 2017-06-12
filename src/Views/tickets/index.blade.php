4f168c7902b5447c943e55fc5a03277d38b2bade
Xavi <xaviqv@tordera.cat>
2017-06-12 14:34:55 +0200

    patch: undo maxLevel usage as property

--- src/Views/tickets/index.blade.php
+++ src/Views/tickets/index.blade.php
@@ -2,7 +2,7 @@
 	<div class="panel-body">
 		<?php $button_create = link_to_route($setting->grab('main_route').'.create', trans('ticketit::lang.btn-create-new-ticket'), null, ['class' => 'btn btn-default pull-right']);?>
 		
-		@if ($u->currentLevel() == 2 and $u->maxLevel == 2)
+		@if ($u->currentLevel() == 2 and $u->maxLevel() == 2)
 			<div class="title pov">{{ trans('ticketit::lang.filter-pov') }}</div>
 			<a href="{{ url($setting->grab('main_route').'/filter/currentLevel/1') }}" id="agent_pov" data-other="user_pov" title="veure com usuari"><button type="button" class="btn btn-default pov-link">{{ trans('ticketit::lang.agent') }}</button></a>			
 		@endif
@@ -12,7 +12,7 @@
 			{!! $button_create !!}
 		@else			
 			<div class="text-center">
-				@if ($u->maxLevel == 2)
+				@if ($u->maxLevel() == 2)
 					<div class="pull-left">
 						<div class="title pov">{{ trans('ticketit::lang.filter-pov') }}</div>
 						<a href="{{ url($setting->grab('main_route').'/filter/currentLevel/remove') }}" id="user_pov" data-other="agent_pov" title="veure com agent"><button type="button" class="btn btn-default pov-link">{{ trans('ticketit::lang.user') }}</button></a>
