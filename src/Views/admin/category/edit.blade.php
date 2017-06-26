f58826b5fb3d5f9ed2360f5916a5d71eccf0f584
Xavi <xaviqv@tordera.cat>
2017-06-26 09:27:49 +0200

    move tag modal div to included file

--- src/Views/admin/category/edit.blade.php
+++ src/Views/admin/category/edit.blade.php
@@ -40,50 +40,12 @@
         @include('ticketit::admin.category.form', ['update', true])
         {!! CollectiveForm::close() !!}
     </div>
-	<div class="modal fade" id="tag-edit-modal" tabindex="-1" role="dialog" aria-labelledby="tag-edit-modal-Label">
-    <div class="modal-dialog model-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">{{ trans('ticketit::lang.flash-x') }}</span></button>
-                <h4 class="modal-title" id="tag-edit-modal-Label">{{ trans('ticketit::admin.category-edit-tag') . trans('ticketit::admin.colon') }} "<span id="jquery_popup_tag_title"></span>"</h4>
-            </div>
-            <div class="modal-body">
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        {!! CollectiveForm::text('name', 'Tag name', ['id'=>'jquery_popup_tag_input', 'class' => 'form-control', 'required']) !!}
-                    </div> 
-				</div>
-				
-				<div class="clearfix"></div>
-
-				<div class="row">
-					<div class="col-sm-6">
-						<h4>{{ trans('ticketit::admin.category-edit-tag-background') }}</h4>
-						<div id="pick_bg" class="colorpickerplus-embed">
-						  <div class="colorpickerplus-container"> </div>
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<h4>{{ trans('ticketit::admin.category-edit-tag-text') }}</h4>
-						<div id="pick_text" class="colorpickerplus-embed">
-						  <div class="colorpickerplus-container"> </div>
-						</div>
-					</div>
-				</div>				
-
-				<div class="modal-footer">					
-					{!! CollectiveForm::button(trans('ticketit::lang.btn-submit'), ['id'=>'jquery_popup_tag_submit', 'class' => 'btn btn-primary']) !!}
-				</div>
-				
-			</div>
-		</div>
-	</div>	
+	@include('ticketit::admin.category.modal-tag')
 	@include('ticketit::admin.category.color_script')
 	@include('ticketit::admin.category.tags_script')
 	<script type="text/javascript">
 	var elem_i="";
 	$(function(){		
-		
 		$('#tag-edit-modal').on('show.bs.modal', function (e)
 		{
 			var button=$(e.relatedTarget);
---
+++ src/Views/admin/category/modal-tag.blade.php
@@ -0,0 +1,39 @@
+<div class="modal fade" id="tag-edit-modal" tabindex="-1" role="dialog" aria-labelledby="tag-edit-modal-Label">
+    <div class="modal-dialog model-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">{{ trans('ticketit::lang.flash-x') }}</span></button>
+                <h4 class="modal-title" id="tag-edit-modal-Label">{{ trans('ticketit::admin.category-edit-tag') . trans('ticketit::admin.colon') }} "<span id="jquery_popup_tag_title"></span>"</h4>
+            </div>
+            <div class="modal-body">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        {!! CollectiveForm::text('name', null, ['id'=>'jquery_popup_tag_input', 'class' => 'form-control', 'required']) !!}
+                    </div> 
+				</div>
+				
+				<div class="clearfix"></div>
+
+				<div class="row">
+					<div class="col-sm-6">
+						<h4>{{ trans('ticketit::admin.category-edit-tag-background') }}</h4>
+						<div id="pick_bg" class="colorpickerplus-embed">
+						  <div class="colorpickerplus-container"> </div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<h4>{{ trans('ticketit::admin.category-edit-tag-text') }}</h4>
+						<div id="pick_text" class="colorpickerplus-embed">
+						  <div class="colorpickerplus-container"> </div>
+						</div>
+					</div>
+				</div>				
+
+				<div class="modal-footer">					
+					{!! CollectiveForm::button(trans('ticketit::lang.btn-submit'), ['id'=>'jquery_popup_tag_submit', 'class' => 'btn btn-primary']) !!}
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
