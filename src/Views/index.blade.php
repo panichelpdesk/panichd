9a442cb8f969716deed9bcb9673dc2733ab296fc
Xavi <xaviqv@tordera.cat>
2017-10-02 12:33:55 +0200

    agent list popover without mouseover

--- src/Views/index.blade.php
+++ src/Views/index.blade.php
@@ -103,20 +103,9 @@
 			});
 			
 			// Agent change: Popover menu
-			$(".jquery_agent_change_integrated").popover({ trigger: "manual" , html: true, animation:false})				
-				.on("mouseenter", function () {
-					var _this = this;
-					$(this).popover("show");
-					$(".popover").on("mouseleave", function () {
-						$(_this).popover('hide');
-					});					
-				}).on("mouseleave", function () {
-					var _this = this;
-					setTimeout(function () {
-						if (!$(".popover:hover").length) {
-							$(_this).popover("hide");
-						}
-					}, 200);					
+			$(".jquery_agent_change_integrated").popover({ html: true})
+			.click(function(e){
+				e.preventDefault();
 			});
 			
 			// Agent change: Popover menu submit
