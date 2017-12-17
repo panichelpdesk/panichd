9cb95eefe81a80f9e5c1124852fb035f91e4110d
XaviQV <xaviqv@gmail.com>
2017-12-17 22:58:53 +0100

    patch: ticket edition error when there are no tags in a category

--- src/Views/tickets/partials/tags_menu.blade.php
+++ src/Views/tickets/partials/tags_menu.blade.php
@@ -1,15 +1,15 @@
 @foreach ($categories as $id=>$category)
-	<select id="jquery_tag_category_{{$id}}" class="jquery_tag_category  select2-multiple" name="category_{{$id}}_tags[]" multiple="multiple" style="display: none; width: 100%">
-	
 	<?php $cat_tags = $tag_lists;
 	$cat_tags = $cat_tags->filter(function($q) use($id){
 		return $q->id==$id;
-	})->first()->tags()->get();
+	})->first();?>
 	
-	?>				
-	@foreach ($cat_tags as $i=>$tag)
-		<option value="{{$tag->id}}" {{ in_array($tag->id, $a_tags_selected)?' selected="selected"':' style=""'}}>{{$tag->name}}</option>
-	@endforeach
+	@if($cat_tags)
+	<select id="jquery_tag_category_{{$id}}" class="jquery_tag_category  select2-multiple" name="category_{{$id}}_tags[]" multiple="multiple" style="display: none; width: 100%">
+		@foreach ($cat_tags->tags()->get() as $i=>$tag)
+			<option value="{{$tag->id}}" {{ in_array($tag->id, $a_tags_selected)?' selected="selected"':' style=""'}}>{{$tag->name}}</option>
+		@endforeach
+	@endif
 	
 	</select>
 @endforeach
