0c40771d68a0e8aeda8091033891b6896788efd3
XaviQV <xaviqv@gmail.com>
2018-02-04 20:55:47 +0100

    Basic Seeder priorities and statuses moved linked files

--- src/Seeds/Basic.php
+++ src/Seeds/Basic.php
@@ -6,36 +6,12 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use PanicHD\PanicHD\Models;
+use PanicHD\PanicHD\Models\Category;
 
 class Basic extends Seeder
 {
     public $categories = [
         'Issues'     => '#ffbc1b',
-    ];
-	
-    public $priorities = [
-        'Critical' => [
-			'color' => '#cc0000',
-			'magnitude' => '4'],
-		'High' => [
-			'color' => '#ffbc1b',
-			'magnitude' => '3'],
-		'Normal' => [
-			'color' => '#aaa',
-			'magnitude' => '2'],
-		'Low' => [
-			'color' => '#4bcd54',
-			'magnitude' => '1'],
-    ];
-	
-	public $statuses = [		
-		'New' => '#319df8',
-		'Open' => '#ffbc1b',
-		'Customer pending' => '#df32f9',
-		'Pending by other' => '#df32f9',
-		'Resolved' => '4bcd540',
-		'Archived' => '#858585',
     ];
 
     /**
@@ -49,24 +25,15 @@ class Basic extends Seeder
 
         // Create categories
         foreach ($this->categories as $name => $color) {
-            $category = Models\Category::firstOrNew(['name'  => $name]);
+            $category = Category::firstOrNew(['name'  => $name]);
 			$category->color = $color;
 			$category->save();
         }
-
-        // Create priorities
-        foreach ($this->priorities as $name => $data) {
-            $priority = Models\Priority::firstOrNew(['name'  => $name]);
-			$priority->color = $data['color'];
-			$priority->magnitude = $data['magnitude'];
-			$priority->save();
-        }
+		
+		// Create priorities
+		$this->call(BasicPriorities::class);
 		
 		// Create statuses
-        foreach ($this->statuses as $name => $color) {
-            $status = Models\Status::firstOrNew(['name'  => $name]);
-			$status->color = $color;
-			$status->save();
-        }
+		$this->call(BasicStatuses::class);
     }
 }
---
+++ src/Seeds/BasicPriorities.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PanicHD\PanicHD\Seeds;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use PanicHD\PanicHD\Models\Priority;
+
+class BasicPriorities extends Seeder
+{
+    public $priorities = [
+        'Critical' => [
+			'color' => '#cc0000',
+			'magnitude' => '4'],
+		'High' => [
+			'color' => '#ffbc1b',
+			'magnitude' => '3'],
+		'Normal' => [
+			'color' => '#aaa',
+			'magnitude' => '2'],
+		'Low' => [
+			'color' => '#4bcd54',
+			'magnitude' => '1'],
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // Create priorities
+        foreach ($this->priorities as $name => $data) {
+            $priority = Priority::firstOrNew(['name'  => $name]);
+			$priority->color = $data['color'];
+			$priority->magnitude = $data['magnitude'];
+			$priority->save();
+        }
+    }
+}
