05d13fcd2a0ee33d9eb9a848d20a76c9abd4c5fb
Flavio Ferreira <flaviometalvale@gmail.com>
2018-02-20 01:19:21 -0300

    PT BR Translation

--- src/Translations/pt_BR/admin.php
+++ src/Translations/pt_BR/admin.php
@@ -2,133 +2,295 @@
 
 return [
 
-    /*
-    *  Constants
-    */
-    'nav-settings'                  => 'Configurações',
-    'nav-agents'                    => 'Agentes',
-    'nav-dashboard'                 => 'Dashboard',
-    'nav-categories'                => 'Categorias',
-    'nav-priorities'                => 'Prioridades',
-    'nav-statuses'                  => 'Status',
-    'nav-configuration'             => 'Configuração',  // New
 
-    'table-hash'                    => '#', // New
-    'table-id'                      => 'ID',
-    'table-name'                    => 'Nome',
-    'table-action'                  => 'Função',
-    'table-categories'              => 'Categorias',
-    'table-join-category'           => 'Categorias Relacionadas',
-    'table-remove-agent'            => 'Remover de Agentes',
+    'nav-settings' => 'Configurações',
+    'nav-agents' => 'Agentes',
+    'nav-dashboard' => 'Dashboard',
 
-    'table-slug'                    => 'Índice', // New
-    'table-default'                 => 'Valor Inicial', // New
-    'table-value'                   => 'Meus Valores',  // New
-    'table-lang'                    => 'Lingua', // New
-    'table-edit'                    => 'Editar', // New
+    'nav-dashboard-title' => 'Painel do administrador',
+    'nav-categories' => 'Categorias',
+    'nav-priorities' => 'Prioridades',
+    'nav-statuses' => 'Status',
 
-    'btn-back'                      => 'Voltar',
-    'btn-delete'                    => 'Excluir',
-    'btn-edit'                      => 'Editar',
-    'btn-join'                      => 'Adicionar',
-    'btn-remove'                    => 'Remover',
-    'btn-submit'                    => 'Enviar',
-    'btn-save'                      => 'Salvar',  // New
-    'btn-update'                    => 'Atualizar',
-    'colon'                         => ': ',
+    'nav-notices' => 'Avisos',
+    'nav-configuration' => 'Configuração',
 
-    /*
-    *  Page specific
-    */
+    'nav-administrator' => 'Administrador',
+    'table-hash' => '#',
+    'table-id' => 'ID',
+    'table-name' => 'Nome',
 
-    // $admin_route_path/dashboard
-    'index-title'                         => 'Sistema de Chamados - Dashboard',
-    'index-empty-records'                 => 'Sem Chamados ainda',
-    'index-total-tickets'                 => 'Total de Chamados',
-    'index-open-tickets'                  => 'Chamados Abertos',
-    'index-closed-tickets'                => 'Chamados Encerrados',
-    'index-performance-indicator'         => 'Indicador de Performance',
-    'index-periods'                       => 'Periodos',
-    'index-3-months'                      => '3 meses',
-    'index-6-months'                      => '6 meses',
-    'index-12-months'                     => '12 meses',
-    'index-tickets-share-per-category'    => 'Chamados Divididos por Categoria',
-    'index-tickets-share-per-agent'       => 'Chamados Divididos por Agentes',
-    'index-categories'                    => 'Categorias',
-    'index-category'                      => 'Categoria',
-    'index-agents'                        => 'Agentes',
-    'index-agent'                         => 'Agente',
-    'index-users'                         => 'Usuários',
-    'index-user'                          => 'Usuário',
-    'index-tickets'                       => 'Chamados',
-    'index-open'                          => 'Aberto',
-    'index-closed'                        => 'Fechado',
-    'index-total'                         => 'Total',
-    'index-month'                         => 'Mês',
-    'index-performance-chart'             => 'Quantos dias você tem para resolver este Chamado?',
-    'index-categories-chart'              => 'Chamados distribuidos por Categoria',
-    'index-agents-chart'                  => 'Chamados distribuidos por Agente',
+    'table-create-level' => 'Criar tickets',
+    'table-action' => 'Função',
+    'table-categories' => 'Categorias',
 
-    // $admin_route_path/agent/____
-    'agent-index-title'             => 'Gerenciar Agentes',
-    'btn-create-new-agent'          => 'Criar Novo Agente',
-    'agent-index-no-agents'         => 'Nenhum Agente cadastrado, ',
-    'agent-index-create-new'        => 'Adicionar agentes',
-    'agent-create-title'            => 'Adicionar Agente',
-    'agent-create-add-agents'       => 'Adicionar Agentes',
-    'agent-create-no-users'         => 'Nenhum Usuário cadastrado, Crie uma conta de Usuário Primeiro.',
-    'agent-create-select-user'      => 'Selecione uma conta de Usuário para criar um Agente',
+    'table-categories-autoasg-title' => 'Nova atribuição automática de tickets',
 
-    // $admin_route_path/category/____
-    'category-index-title'          => 'Gerenciar Categorias',
-    'btn-create-new-category'       => 'Criar Nova Categoria',
-    'category-index-no-categories'  => 'Nenhuma Categoria cadastrada, ',
-    'category-index-create-new'     => 'Criar Nova Categoria',
-    'category-index-js-delete'      => 'Você tem certeza que deseja excluir esta Categoria: ',
-    'category-create-title'         => 'Criar Nova Categoria',
-    'category-create-name'          => 'Nome',
-    'category-create-color'         => 'Cor',
-    'category-edit-title'           => 'Editar Categoria: :name',
+    'table-magnitude' => 'Magnitude',
 
-    // $admin_route_path/priority/____
-    'priority-index-title'          => 'Gerenciar Prioridades',
-    'btn-create-new-priority'       => 'Criar Nova Prioridade',
-    'priority-index-no-priorities'  => 'Nenhuma Prioridade cadastrada, ',
-    'priority-index-create-new'     => 'criar nova prioridade',
-    'priority-index-js-delete'      => 'Você tem certeza que deseja excluir esta prioridade: ',
-    'priority-create-title'         => 'Criar Nova Prioridade',
-    'priority-create-name'          => 'Nome',
-    'priority-create-color'         => 'Cor',
-    'priority-edit-title'           => 'Editar Prioridade: :name',
+    'table-num-tickets' => 'Contagem de tickets',
+    'table-remove-agent' => 'Remover de Agentes',
 
-    // $admin_route_path/status/____
-    'status-index-title'            => 'Gerenciar Status',
-    'btn-create-new-status'         => 'Criar Novo status',
-    'status-index-no-statuses'      => 'Nenhum Status cadastrado,',
-    'status-index-create-new'       => 'criar novo status',
-    'status-index-js-delete'        => 'Você tem certeza que deseja excluir este status: ',
-    'status-create-title'           => 'Criar Novo Status',
-    'status-create-name'            => 'Nome',
-    'status-create-color'           => 'Cor',
-    'status-edit-title'             => 'Editar Status: :name',
+    'table-remove-administrator' => 'Remover dos administradores',
+    'table-slug' => 'Índice',
+    'table-default' => 'Valor Inicial',
+    'table-value' => 'Meus Valores',
+    'table-lang' => 'Lingua',
+    'table-edit' => 'Editar',
+    'btn-back' => 'Voltar',
 
-    // $admin_route_path/configuration/____
-    'config-index-title'            => 'Gerenciar Configurações', // New
-    'config-index-subtitle'         => 'Configurações', // New
-    'btn-create-new-config'         => 'Adicionar Nova Configuração', // New
-    'config-index-no-settings'      => 'Nenhuma Configuração, ', // New
-    'config-index-initial'          => 'Inicial', // New
-    'config-index-tickets'          => 'Chamados', // New
-    'config-index-notifications'    => 'Notificações', // New
-    'config-index-permissions'      => 'Permissões', // New
-    'config-index-other'            => 'Outras', // New
-    'config-create-title'           => 'Criar: Nova Configuração Global', // New
-    'config-create-subtitle'        => 'Criar Configuração', // New
-    'config-edit-title'             => 'Editar: Configuração Global', // New
-    'config-edit-subtitle'          => 'Editar Configuração', // New
-    'config-edit-id'                => 'ID',
-    'config-edit-slug'              => 'Índice',
-    'config-edit-default'           => 'Valor Inicial',
-    'config-edit-value'             => 'Meu Valor',
-    'config-edit-language'          => 'Lingua',
+    'btn-change' => 'mudança',
+
+    'btn-create' => 'Crio',
+    'btn-delete' => 'Excluir',
+    'btn-edit' => 'Editar',
+    'btn-join' => 'Adicionar',
+    'btn-remove' => 'Remover',
+    'btn-submit' => 'Enviar',
+    'btn-save' => 'Salvar',
+    'btn-update' => 'Atualizar',
+    'colon' => ': ',
+
+    'level-1' => 'Todos',
+
+    'level-2' => 'agentes atribuídos + administradores.',
+
+    'level-3' => 'administradores.',
+    'index-title' => 'Sistema de Chamados - Dashboard',
+    'index-empty-records' => 'Sem Chamados ainda',
+    'index-total-tickets' => 'Total de Chamados',
+    'index-open-tickets' => 'Chamados Abertos',
+    'index-closed-tickets' => 'Chamados Encerrados',
+    'index-performance-indicator' => 'Indicador de Performance',
+    'index-periods' => 'Periodos',
+    'index-3-months' => '3 meses',
+    'index-6-months' => '6 meses',
+    'index-12-months' => '12 meses',
+    'index-tickets-share-per-category' => 'Chamados Divididos por Categoria',
+    'index-tickets-share-per-agent' => 'Chamados Divididos por Agentes',
+    'index-categories' => 'Categorias',
+    'index-category' => 'Categoria',
+    'index-agents' => 'Agentes',
+    'index-agent' => 'Agente',
+
+    'index-administrators' => 'Administradores',
+
+    'index-administrator' => 'Administrador',
+    'index-users' => 'Usuários',
+    'index-user' => 'Usuário',
+    'index-tickets' => 'Chamados',
+    'index-open' => 'Aberto',
+    'index-closed' => 'Fechado',
+    'index-total' => 'Total',
+    'index-month' => 'Mês',
+    'index-performance-chart' => 'Quantos dias você tem para resolver este Chamado?',
+    'index-categories-chart' => 'Chamados distribuidos por Categoria',
+    'index-agents-chart' => 'Chamados distribuidos por Agente',
+    'agent-index-title' => 'Gerenciar Agentes',
+    'agent-index-no-agents' => 'Nenhum Agente cadastrado, ',
+    'agent-index-create-new' => 'Adicionar agentes',
+
+    'agent-create-form-agent' => 'Do utilizador',
+    'agent-create-add-agents' => 'Adicionar Agentes',
+    'agent-create-no-users' => 'Nenhum Usuário cadastrado, Crie uma conta de Usuário Primeiro.',
+
+    'agent-store-ok' => 'Usuário ":name" foi adicionado aos agentes',
+
+    'agent-updated-ok' => 'Agente ":name" Atualizado com sucesso',
+
+    'agent-excluded-ok' => 'Agente ":name" excluído dos agentes',
+
+    'agent-store-error-no-category' => 'Para adicionar um agente, você deve verificar pelo menos uma categoria',
+
+    'agent-edit-title' => 'Permissões de usuário :agent',
+
+    'agent-edit-table-category' => 'Categoria',
+
+    'agent-edit-table-agent' => 'Permissão do agente',
+
+    'agent-edit-table-autoassign' => 'Novos tickets auto.',
+
+    'administrator-index-title' => 'Gerenciamento de Administradores',
+
+    'btn-create-new-administrator' => 'Criar novo administrador',
+
+    'administrator-index-no-administrators' => 'Não há administradores,',
+
+    'administrator-index-create-new' => 'Adicionar administradores',
+
+    'administrator-create-title' => 'Adicionar administrador',
+
+    'administrator-create-add-administrators' => 'Adicionar Administradores',
+
+    'administrator-create-no-users' => 'Não há contas de usuário, crie contas de usuário primeiro.',
+
+    'administrator-create-select-user' => 'Selecione as contas de usuário a serem adicionadas como administradores',
+    'category-index-title' => 'Gerenciar Categorias',
+    'btn-create-new-category' => 'Criar Nova Categoria',
+    'category-index-no-categories' => 'Nenhuma Categoria cadastrada, ',
+    'category-index-create-new' => 'Criar Nova Categoria',
+    'category-index-js-delete' => 'Você tem certeza que deseja excluir esta Categoria: ',
+
+    'category-index-email' => 'Email de notificações',
+
+    'category-index-reasons' => 'Razões de fechamento',
+
+    'category-index-tags' => 'Tag',
+    'category-create-title' => 'Criar Nova Categoria',
+    'category-create-name' => 'Nome',
+
+    'category-create-email' => 'Email de notificações',
+
+    'category-email-origin' => 'Origem',
+
+    'category-email-origin-website' => 'Local na rede Internet',
+
+    'category-email-origin-tickets' => 'E-mail geral dos tickets',
+
+    'category-email-origin-category' => 'Esta categoria',
+
+    'category-email-from' => 'A partir de',
+
+    'category-email-name' => 'Nome',
+
+    'category-email' => 'O email',
+
+    'category-email-reply-to' => 'Responder a',
+
+    'category-email-default' => 'Padrão',
+
+    'category-email-this' => 'Esta caixa de correio',
+
+    'category-email-from-info' => 'Remetente de correio usado em todas as notificações nesta categoria',
+
+    'category-email-reply-to-info' => 'E-mail de destino para notificações de e-mail de notificação',
+
+    'category-email-reply-this-info' => 'O especificado abaixo',
+    'category-create-color' => 'Cor',
+
+    'category-create-new-tickets' => 'Quem pode criar tickets',
+
+    'category-create-new-tickets-help' => 'Nível mínimo para criar tickets na categoria',
+    'category-edit-title' => 'Editar Categoria: :name',
+
+    'category-edit-closing-reasons' => 'Razões de fechamento de tickets',
+
+    'category-edit-closing-reasons-help' => 'Opções que o usuário pode escolher ao fechar o ticket',
+
+    'category-edit-reason' => 'Razão de fechamento',
+
+    'category-edit-reason-label' => 'Razão',
+
+    'category-edit-reason-status' => 'Status',
+
+    'category-delete-reason' => 'Excluir motivo',
+
+    'category-edit-new-tags' => 'Novas tags',
+
+    'category-edit-current-tags' => 'Etiquetas atuais',
+
+    'category-edit-tag' => 'Editar tag',
+
+    'category-edit-tag-background' => 'fundo',
+
+    'category-edit-tag-text' => 'Texto',
+    'priority-index-title' => 'Gerenciar Prioridades',
+
+    'priority-index-help' => 'Você pode alterar a ordem de prioridade arrastando as linhas da tabela. Este pedido será usado também na lista de tickets ao verificar este campo',
+    'btn-create-new-priority' => 'Criar Nova Prioridade',
+    'priority-index-no-priorities' => 'Nenhuma Prioridade cadastrada, ',
+    'priority-index-create-new' => 'criar nova prioridade',
+    'priority-index-js-delete' => 'Você tem certeza que deseja excluir esta prioridade: ',
+    'priority-create-title' => 'Criar Nova Prioridade',
+    'priority-create-name' => 'Nome',
+    'priority-create-color' => 'Cor',
+    'priority-edit-title' => 'Editar Prioridade: :name',
+
+    'priority-delete-title' => 'Eliminar prioridade: :name',
+
+    'priority-delete-warning' => 'Há < span class=" modal-tickets-count " </span> tickets que usam essa prioridade. Você deve escolher outro para todos eles',
+
+    'priority-delete-error-no-priority' => 'Você precisa especificar uma nova prioridade para ":name" Bilhetes relacionados com prioridade',
+    'status-index-title' => 'Gerenciar Status',
+    'btn-create-new-status' => 'Criar Novo status',
+    'status-index-no-statuses' => 'Nenhum Status cadastrado,',
+    'status-index-create-new' => 'criar novo status',
+    'status-index-js-delete' => 'Você tem certeza que deseja excluir este status: ',
+    'status-create-title' => 'Criar Novo Status',
+    'status-create-name' => 'Nome',
+    'status-create-color' => 'Cor',
+    'status-edit-title' => 'Editar Status: :name',
+
+    'status-delete-title' => 'Excluir status ":name"',
+
+    'status-delete-warning' => 'Há < span class=" modal-tickets-count " </span> tickets que usam esse status. Você deve escolher outro para todos eles',
+
+    'status-delete-error-no-status' => 'Você precisa especificar um novo status para ":name" tickets relacionados ao status',
+
+    'notice-index-title' => 'Avisos para gerenciamento de departamentos',
+
+    'btn-create-new-notice' => 'Adicionar aviso',
+
+    'notice-index-empty' => 'Não há avisos configurados.',
+
+    'notice-index-owner' => 'Proprietário',
+
+    'notice-index-email' => 'Aviso de e-mail',
+
+    'notice-index-department' => 'Aviso visível para',
+
+    'notice-modal-title-create' => 'Adicione um aviso ao departamento',
+
+    'notice-modal-title-update' => 'Atualize um aviso ao departamento',
+
+    'notice-saved-ok' => 'Aviso salvo corretamente',
+
+    'notice-deleted-ok' => 'Aviso apagado',
+
+    'notice-index-js-delete' => 'Tem certeza de que deseja excluir este aviso?',
+
+    'notice-index-help' => 'Quando um conjunto de tickets com um dos seguintes proprietários é criado, haverá duas coisas: < br / > < br / > < ol > < li > Um e-mail será enviado para ticket < B > proprietário </b>, com um modelo de e-mail específico. < / li > < li > Enquanto o ticket for <b> open </b>, os usuários no mesmo departamento verão o ticket como aviso <b> aviso </b> no menu Criar ticket.',
+
+    'notice-index-owner-alert' => 'Um usuário normal, ao criar um novo ticket, não poderá ver nenhum usuário listado aqui',
+    'config-index-title' => 'Gerenciar Configurações',
+    'config-index-subtitle' => 'Configurações',
+    'btn-create-new-config' => 'Adicionar Nova Configuração',
+    'config-index-no-settings' => 'Nenhuma Configuração, ',
+    'config-index-initial' => 'Inicial',
+    'config-index-tickets' => 'Chamados',
+    'config-index-notifications' => 'Notificações',
+    'config-index-permissions' => 'Permissões',
+
+    'config-index-editor' => 'editor',
+    'config-index-other' => 'Outras',
+    'config-create-title' => 'Criar: Nova Configuração Global',
+    'config-create-subtitle' => 'Criar Configuração',
+    'config-edit-title' => 'Editar: Configuração Global',
+    'config-edit-subtitle' => 'Editar Configuração',
+    'config-edit-id' => 'ID',
+    'config-edit-slug' => 'Índice',
+    'config-edit-default' => 'Valor Inicial',
+    'config-edit-value' => 'Meu Valor',
+    'config-edit-language' => 'Lingua',
+
+    'config-edit-unserialize' => 'Obter os valores da matriz e alterar os valores',
+
+    'config-edit-serialize' => 'Obter a seqüência serializada dos valores alterados (para ser inserido no campo)',
+
+    'config-edit-should-serialize' => 'Serialize',
+
+    'config-edit-eval-warning' => 'Quando marcado, o servidor executará eval ()!
+Não use isso se eval () estiver desativado em seu servidor ou se você não sabe exatamente o que está fazendo!
+Código exato executado:',
+
+    'config-edit-reenter-password' => 'Digite sua senha novamente',
+
+    'config-edit-auth-failed' => 'Senha incorreta',
+
+    'config-edit-eval-error' => 'Valor inválido',
+
+    'config-edit-tools' => 'Ferramentas:',
+
+    'config-update-confirm' => 'Configuração :name foi atualizado',
+
 ];
