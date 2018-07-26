783b34e496a66df016fd37b57fbf2da5946a0665
Xavi <xaviqv@tordera.cat>
2018-07-26 11:09:32 +0200

    Notices: Improved calendar information

--- src/Views/notices/widget.blade.php
+++ src/Views/notices/widget.blade.php
@@ -11,7 +11,9 @@
 			@foreach ($a_notices as $notice)
 				<tr>
 				<td style="width: 14em;">{{ $notice->id }}<br /><span style="font-weight: bold">{{ $notice->status->name }}</span>
-				<div style="margin: 1em 0em 0em 0em; font-weight: bold;"><span class="glyphicon glyphicon-calendar"></span> {!! $notice->getDateForHumans('limit_date', isset($text_dates) && $text_dates ? true : false) !!}</div>
+				<div style="margin: 1em 0em 0em 0em; font-weight: bold;">
+				<span class="tooltip-info" title="{{ date(trans('panichd::lang.date-format'), strtotime(is_null($notice->limit_date) ? $notice->start_date : $notice->limit_date)) . ", " . $notice->getTime('limit_date') }}" data-toggle="tooltip" data-placement="auto bottom"><span class="glyphicon glyphicon-calendar"></span> {!! $notice->getDateForHumans('limit_date', true) !!}</span>
+				</div>
 				
 				</td>
 				<td><div style="margin: 0em 0em 1em 0em;">{{ link_to_route($setting->grab('main_route').'.show', $notice->subject, $notice->id) }}</div>
