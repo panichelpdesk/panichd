138f19bfc708b398bf508d503922898d4fcf2e11
Xavi <xaviqv@tordera.cat>
2018-08-17 14:31:55 +0200

    Improve HTML input to avoid unexpected end tag "p" exception in some cases

--- src/Views/emails/partial/both_html_fields.blade.php
+++ src/Views/emails/partial/both_html_fields.blade.php
@@ -1,10 +1,10 @@
 <b>{{ trans('panichd::lang.description') }}</b>
 <table border="0" cellpadding="10" cellspacing="0" style="border: 1px solid #ddd; border-radius: 5px;"><tr>
-	<td>@include('panichd::emails.partial.html_field', ['html_field' => $ticket->html])</td>
+	<td>@include('panichd::emails.partial.html_field', ['html_field' => $ticket->html, 'ticket' => $ticket, 'field' => 'description'])</td>
 </tr></table><br /><br />
 @if ($ticket->intervention_html)
 	<b>{{ trans('panichd::lang.intervention') }}</b>
 	<table border="0" cellpadding="10" cellspacing="0" style="border: 1px solid #ddd; border-radius: 5px;"><tr>
-		<td>@include('panichd::emails.partial.html_field', ['html_field' => $ticket->intervention_html])</td>
+		<td>@include('panichd::emails.partial.html_field', ['html_field' => $ticket->intervention_html, 'ticket' => $ticket, 'field' => 'intervention'])</td>
 	</tr></table><br /><br />
 @endif
