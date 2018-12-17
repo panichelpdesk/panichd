08ed6a5631bdd07eea140ad2cad79c286288903c
Xavi <xaviqv@tordera.cat>
2018-12-17 12:15:46 +0100

    Allow any filter for filteronly

--- src/Controllers/FiltersController.php
+++ src/Controllers/FiltersController.php
@@ -15,17 +15,14 @@ class FiltersController extends Controller
     use CacheVars, TicketFilters;
 
 	// $a_filters is in TicketFilters trait
-	
-	// Filters that can be called via /filteronly/
-	private $a_only_filters = ['category', 'owner', 'agent'];
-	
+
 	// Ticket list names and related route name
 	private $a_lists = [
 		'newest' => '-newest',
 		'active' => '.index',
 		'complete' => '-complete'
 	];
-	
+
 	/*
 	 * Update a single filter
 	*/
@@ -37,10 +34,10 @@ class FiltersController extends Controller
             if ($value == 'remove') {
                 // Delete filter
                 $request->session()->forget('panichd_filter_'.$filter);
-				
+
 				// Check all filters
 				$this->validateFilters($request);
-				
+
             } else {
                 // Validate and add a filter
 				$this->addAFilter($request, $filter, $value);
@@ -49,29 +46,29 @@ class FiltersController extends Controller
 
         return \Redirect::back();
     }
-	
+
 	/*
 	 * Delete all filters and apply only the selected one
 	*/
 	public function only(Request $request, $filter, $value, $list)
 	{
-		if (in_array($filter, $this->a_only_filters) and in_array($list, array_keys($this->a_lists))){
-			
+		if (in_array($list, array_keys($this->a_lists))){
+
 			// Delete each filter from session
 			foreach ($this->a_filters as $delete){
 				$request->session()->forget('panichd_filter_'.$delete);
 			}
-			
+
 			// Validate and add a filter
 			if ($this->addAFilter($request, $filter, $value)){
 				// Redirect to specified route
 				return redirect()->route(Setting::grab('main_route').$this->a_lists[$list]);
 			}
 		}
-		
+
 		return \Redirect::back();
 	}
-	
+
 	/*
 	 * Add a filter and validate it
 	 *
@@ -81,18 +78,18 @@ class FiltersController extends Controller
 	{
 		// Add filter
 		$request->session()->put('panichd_filter_'.$filter, $value);
-		
+
 		// Check all filters
 		list($request, $filters_count) = $this->validateFilters($request);
-	   
+
 		if ($filters_count > 0) {
 			// General filter check
 			$request->session()->put('panichd_filters','yes');
 		}
-		
+
 		return $request->session()->exists('panichd_filter_'.$filter);
 	}
-	
+
 	/*
 	 * Remove all filters
 	*/
@@ -102,15 +99,15 @@ class FiltersController extends Controller
 		foreach ($this->a_filters as $filter){
 			$request->session()->forget('panichd_filter_'.$filter);
 		}
-		
+
 		// General filter uncheck
 		$request->session()->forget('panichd_filters');
-		
+
 		if ($list != "" and array_key_exists($list, $this->a_lists)){
 			// Redirect to specified route
 			return redirect()->route(Setting::grab('main_route').$this->a_lists[$list]);
 		}
-		
+
 		return \Redirect::back();
 	}
 }
