2e8acf9ef592518801f08f8e8d94aff647f8c670
Xavi <xaviqv@tordera.cat>
2018-12-17 13:44:14 +0100

    Add filterjust route to allow setting many filters by url

--- src/Controllers/FiltersController.php
+++ src/Controllers/FiltersController.php
@@ -69,6 +69,66 @@ class FiltersController extends Controller
 		return \Redirect::back();
 	}
 
+    /*
+	 * Delete all previous filters and apply just the valid ones from the requested
+     * You may specify a ticket list as a final parameter (like URL/complete) o as key/value (like URL/list/complete)
+	*/
+    public function just(Request $request, $parameters)
+    {
+        // Get URL parameters and replace a_current array with them
+        $a_temp = explode('/', $parameters);
+        $a_valid = [];
+        $list = false;
+
+        $i = 0;
+        $filter = "";
+        foreach($a_temp as $param){
+            $i++;
+            if ($filter == ""){
+                if (!$list and $i == count($a_temp) and array_key_exists($param, $this->a_lists)){
+                    // Allow ticket list as a last parameter if not set as list/list_key in previous parameters
+                    $list = $param;
+                }else
+                    $filter = $param;
+            }else{
+                if(in_array($filter, $this->a_filters)){
+                    // Add filter to validate
+                    $a_valid[$filter] = $param;
+                }elseif($filter == 'list' and array_key_exists($param, $this->a_lists)){
+                    // Select ticket list
+                    $list = $param;
+                }
+                $filter = "";
+            }
+        }
+
+        // Delete each filter from session
+        foreach ($this->a_filters as $delete){
+            $request->session()->forget('panichd_filter_'.$delete);
+        }
+
+        if ($a_valid){
+            foreach ($a_valid as $filter => $value){
+                // Add filter
+        		$request->session()->put('panichd_filter_'.$filter, $value);
+            }
+
+            // Check all filters
+    		list($request, $filters_count) = $this->validateFilters($request);
+
+    		if ($filters_count > 0) {
+    			// General filter check
+    			$request->session()->put('panichd_filters','yes');
+    		}
+        }
+
+        if ($list){
+            // Redirect to specified list
+            return redirect()->route(Setting::grab('main_route') . $this->a_lists[$list]);
+        }else
+            return redirect()->route(Setting::grab('main_route') . '.index');
+    }
+
 	/*
 	 * Add a filter and validate it
 	 *
