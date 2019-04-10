198475ac44a4fa24f71cbcab19f95339d444edc2
Xavi <xaviqv@tordera.cat>
2019-04-10 13:40:50 +0200

    Patch: Apply html_replacements in any notification

---
+++ src/Helpers/Purifiable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PanicHD\PanicHD\Helpers;
+
+use PanicHD\PanicHD\Traits\Purifiable as Main;
+
+/**
+ * This file containes the version of different external JS and CSS libraries
+ * that are included.
+ *
+ * Class Cdn
+ */
+class Purifiable
+{
+    use Main;
+
+}
