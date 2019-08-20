dc1d30dd80911379950bad72ccbb850f1e5f720a
Xavi <xaviqv@tordera.cat>
2019-08-20 11:23:55 +0200

    Bug: Page titles were not being applied

--- src/Views/master.blade.php
+++ src/Views/master.blade.php
@@ -8,7 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('page')</title>
+
+    @section('page', config('app.name', 'Laravel'))
 
     <!-- Scripts -->
     <script>
