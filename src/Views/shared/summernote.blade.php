c01b14427223aa31fbb25d10652cefa863503ed0
Xavi <xaviqv@tordera.cat>
2019-09-02 13:01:21 +0200

    Summernote from 0.8.10 to 0.8.12 and moved to named folder

--- src/Views/shared/summernote.blade.php
+++ src/Views/shared/summernote.blade.php
@@ -1,6 +1,6 @@
 @section('panichd_assets')
     @if($editor_enabled)
-        <link rel="StyleSheet" href="{{asset('vendor/panichd/css/summernote/summernote-' . PanicHD\PanicHD\Helpers\Cdn::Summernote .'-bs4.css')}}">
+        <link rel="StyleSheet" href="{{asset('vendor/panichd/summernote/summernote-' . PanicHD\PanicHD\Helpers\Cdn::Summernote .'.bs4.css')}}">
         @if($codemirror_enabled)
             <link rel="StyleSheet" href="{{asset('vendor/panichd/css/codemirror/codemirror-' . PanicHD\PanicHD\Helpers\Cdn::CodeMirror . '.min.css')}}">
             <link rel="StyleSheet" href="{{asset('vendor/panichd/css/codemirror/codemirror-' . PanicHD\PanicHD\Helpers\Cdn::CodeMirror . '-' . $codemirror_theme . '.min.css')}}">
@@ -13,8 +13,8 @@
         <script src="{{asset('vendor/panichd/js/codemirror/codemirror-' . PanicHD\PanicHD\Helpers\Cdn::CodeMirror . '.min.js')}}"></script>
     @endif
 
-    <script src="{{ asset('vendor/panichd/js/summernote/summernote-' . PanicHD\PanicHD\Helpers\Cdn::Summernote .'-bs4.min.js') }}"></script>
+    <script src="{{ asset('vendor/panichd/summernote/summernote-' . PanicHD\PanicHD\Helpers\Cdn::Summernote .'.bs4.min.js') }}"></script>
     @if($editor_locale)
-        <script src="{{ asset('vendor/panichd/js/summernote/lang/summernote-'.$editor_locale.'.js') }}"></script>
+        <script src="{{ asset('vendor/panichd/summernote/lang/summernote-'.$editor_locale.'.js') }}"></script>
     @endif
 @append
