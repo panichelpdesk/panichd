ed5f15432349ae533dbbc8a578744e66788d0cbc
Xavi <xaviqv@tordera.cat>
2019-09-04 12:59:57 +0200

    Migration for new unread ticket or comment field

---
+++ src/Migrations/2019_09_04_124132_add_unread_fields.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUnreadFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('panichd_tickets', function (Blueprint $table) {
+            $table->integer('read_by_agent')->after('agent_id')->default('1');
+        });
+
+        Schema::table('panichd_comments', function (Blueprint $table) {
+            $table->integer('read_by_agent')->after('ticket_id')->default('1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('panichd_tickets', function (Blueprint $table) {
+            $table->dropColumn('read_by_agent');
+        });
+
+        Schema::table('panichd_comments', function (Blueprint $table) {
+            $table->dropColumn('read_by_agent');
+        });
+    }
+}
