922423efca7887985fafe264348b588e59de1522
Xavi <xaviqv@tordera.cat>
2019-09-23 15:17:19 +0200

    Patch: Load some ticket table events once

--- src/Views/tickets/datatable/events.blade.php
+++ src/Views/tickets/datatable/events.blade.php
@@ -1,35 +1,33 @@
 <script>
-$(function(){
-    $('#tickets-table').on('draw.dt', function(e){
+	$(function(){
+		// Plus / less buttons for text fields
+		$(document).on('click', '.jquery_ticket_text_toggle', function(e){
+			var remove = $(this).find('span.fa').hasClass("fa-plus") ? 'plus' : 'minus';
+			var action = $(this).find('span.fa').hasClass("fa-plus") ? 'minus' : 'plus';
+			var id = $(this).data('id');
 
-	    // Plus / less buttons for text fields
-        $('.jquery_ticket_text_toggle').click(function(e){
-            var remove = $(this).find('span.fa').hasClass("fa-plus") ? 'plus' : 'minus';
-            var action = $(this).find('span.fa').hasClass("fa-plus") ? 'minus' : 'plus';
-            var id = $(this).data('id');
+			$(this).closest('tr').find('td').first().effect('highlight');
 
-            $(this).closest('tr').find('td').first().effect('highlight');
+			$('.jquery_ticket_' + id + '_text').each(function(){
+				if (action == 'minus'){
+					$(this).prop('data-height-minus', $(this).height());
+					$(this).find('span.fa').removeClass('fa-plus').addClass('fa-minus');
+					$(this).find('.text_minus').hide();
+					$(this).find('.text_plus').css('display', 'inline');
 
-            $('.jquery_ticket_' + id + '_text').each(function(){
-                if (action == 'minus'){
-                    $(this).prop('data-height-minus', $(this).height());
-                    $(this).find('span.fa').removeClass('fa-plus').addClass('fa-minus');
-                    $(this).find('.text_minus').hide();
-                    $(this).find('.text_plus').css('display', 'inline');
-
-                    $(this).prop('data-height-plus', $(this).height());
-                    $(this).css('height', $(this).prop('data-height-minus')).animate({height: $(this).prop('data-height-plus')}, 500);
-                }else{
-                    $(this).find('span.fa').removeClass('fa-minus').addClass('fa-plus');
-                    $(this).find('.text_minus').show();
-                    $(this).find('.text_plus').hide();
-                    $(this).css('height', '');
-                }
-            });
-        });
+					$(this).prop('data-height-plus', $(this).height());
+					$(this).css('height', $(this).prop('data-height-minus')).animate({height: $(this).prop('data-height-plus')}, 500);
+				}else{
+					$(this).find('span.fa').removeClass('fa-minus').addClass('fa-plus');
+					$(this).find('.text_minus').show();
+					$(this).find('.text_plus').hide();
+					$(this).css('height', '');
+				}
+			});
+		});
 
 		// Agent change: Modal for > 4 agents
-		$('.jquery_agent_change_modal').click(function(e){
+		$(document).on('click', '.jquery_agent_change_modal', function(e){
 			e.preventDefault();
 
 			// Row hover
@@ -43,20 +41,6 @@ $(function(){
 			$('#modalAgentChange .categories_agent_change').hide();
 			$('#modalAgentChange #category_'+$(this).attr('data-category-id')+'_agents').show()
 				.find(":radio[value="+$(this).attr('data-agent-id')+"]").prop('checked',true);
-		});
-
-		$('#modalAgentChange').on('hidden.bs.modal', function () {
-			$(document).find('tr').removeClass('hover');
-		});
-
-		// Agent / Priority change: Popover menu
-		$(".jquery_popover")
-			.popover({
-				html: true,
-				sanitize: false
-			})
-		.click(function(e){
-			e.preventDefault();
 		});
 
 		// Agent change: Popover menu submit
@@ -96,9 +80,8 @@ $(function(){
 
 				success: function( response ) {
 					success_popover(response);
-                }
+				}
 			});
-
 		});
 
 		// Popover submit (Priority or Status)
@@ -129,46 +112,62 @@ $(function(){
 
 				success: function( response ) {
 					success_popover(response);
-                }
+				}
 			});
 		});
 
+		// After every datatable draw
+		$('#tickets-table').on('draw.dt', function(e){
+			// On hidden agent modal
+			$('#modalAgentChange').on('hidden.bs.modal', function () {
+				$(document).find('tr').removeClass('hover');
+			});
 
-		/*
-		 * Common AJAX success response for ticket changes within datatable
-		*/
-		function success_popover(response)
-		{
-			// Show bottom message
-			$('#bottom_toast').empty().append('<div class="alert alert-' + (response.result == 'ok' ? 'info' : 'danger') + '">' + response.message + '</div>');
-			$('#bottom_toast').addClass('show');
-			
-			if(response.result == 'ok'){
-				// Hide any existent popover
-				$(".jquery_popover").popover('hide');
-			}
+			// Agent / Priority change: Popover menu
+			$(".jquery_popover")
+				.popover({
+					html: true,
+					sanitize: false
+				})
+			.click(function(e){
+				e.preventDefault();
+			});
 
-			if(response.result == 'ok' || last_update != response.last_update){
-				// Reload datatable
-				datatable.ajax.reload();
+			// Agent: Tooltip when there is only 1 agents
+			$(".tooltip-info").tooltip();
+		});
 
-				// Apply new last update refference
-				last_update = response.last_update;
-			}
-
-			// Restart check interval
-			init_check_last_update();
-
-			// Hide bottom message
-			setTimeout(function(){
-				$('#bottom_toast').removeClass('show');
-			}, 4000);
-		}
-
-		// Agent: Tooltip when there is only 1 agents
-		$(".tooltip-info").tooltip();
+		@yield('footer_jquery')
 	});
 
-	@yield('footer_jquery')
-});
+	/*
+	* Common AJAX success response for ticket changes within datatable
+	*/
+	function success_popover(response)
+	{
+		// Show bottom message
+		$('#bottom_toast').empty().append('<div class="alert alert-' + (response.result == 'ok' ? 'info' : 'danger') + '">' + response.message + '</div>');
+		$('#bottom_toast').addClass('show');
+		
+		if(response.result == 'ok'){
+			// Hide any existent popover
+			$(".jquery_popover").popover('hide');
+		}
+
+		if(response.result == 'ok' || last_update != response.last_update){
+			// Reload datatable
+			datatable.ajax.reload();
+
+			// Apply new last update refference
+			last_update = response.last_update;
+		}
+
+		// Restart check interval
+		init_check_last_update();
+
+		// Hide bottom message
+		setTimeout(function(){
+			$('#bottom_toast').removeClass('show');
+		}, 4000);
+	}
 </script>
